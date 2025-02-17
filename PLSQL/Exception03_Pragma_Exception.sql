c5c99f48f9eda5d78a91fcc3d65ded3cb819825e
elavarasu86 <94145348+elavarasu86@users.noreply.github.com>
2025-02-16 21:21:26 -0500

    Update Exception03_Pragma_Exception.sql

--- PLSQL/Exception03_Pragma_Exception.sql
+++ PLSQL/Exception03_Pragma_Exception.sql
@@ -2,6 +2,24 @@
 --Associating a name for un-known exception
 
 DECLARE
+    myex EXCEPTION;
+    PRAGMA EXCEPTION_INIT(myex,-20015); 
+    n NUMBER := &n;
+BEGIN
+    FOR i IN 1..n LOOP
+        dbms_output.put.line(i);
+        IF i=n THEN
+            RAISE myex;
+        END IF;
+    END LOOP;
+EXCEPTION
+    WHEN myex THEN
+        dbms_output.put.line('loop finish');
+END;
+/
+**********************************************
+
+DECLARE
     v_num NUMBER;
     e_value_greater_than_limit EXCEPTION;
     PRAGMA EXCEPTION_INIT(e_value_greater_than_limit, -01438);
