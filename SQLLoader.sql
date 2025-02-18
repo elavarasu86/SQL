01e571a6044ceb6f36a5326bb4792596d850dc77
elavarasu86 <94145348+elavarasu86@users.noreply.github.com>
2025-02-18 16:41:49 -0500

    Update SQLLoader.sql

--- SQLLoader.sql
+++ SQLLoader.sql
@@ -27,3 +27,29 @@ when dept_no="10" /*This statement help us to load data which has dept number as
 	
 SQL loader comman:
 sqlldr devuser2/devuser2@orclpdb control=control_file.ctl
+
+************************************************************
+ExternalTable:
+	
+External table is another way to load data from a file into Oracle.
+
+Table will not hold any information(Data), instead it will hit and fetch records from file when we use external table in our queries.
+
+Before creating external table we have to create directory object in Oracle database.
+
+CREATE directory external_data_directory AS 'C:/Oracle/'; 
+
+CREATE TABLE table_name_employee
+  (
+     empno  NUMBER(4),
+     ename  VARCHAR2(10),
+     deptno NUMBER(2)
+  ) organization external( TYPE oracle_loader DEFAULT directory
+external_data_directory ACCESS parameters( records delimited BY newline fields
+terminated BY ',') location('filename.txt'); 
+
+select * from table_name_employee;
+
+In-line external table:
+We don't have to create table instead we can give all the info as part of select clause.
+Note: External Table can be used only from the machine where Oracle is installed not from client system.
