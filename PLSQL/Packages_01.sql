97051c49f0d150a5dd3daa5f05bd9ed966529c78
elavarasu86 <94145348+elavarasu86@users.noreply.github.com>
2025-02-24 11:00:03 -0500

    Add files via upload

---
+++ PLSQL/Packages_01.sql
@@ -0,0 +1,13 @@
+--Packages:
+
+--Advantage 
+	1. Logical grouping of subprograms
+		Logically group the related subprograms, objects and variables.
+	2. Security
+		Security of code through implementation of private subprograms.
+	3. Better Performance
+		Entire package is loaded into memory when the pakcage is first referenced.
+		There is only one copy of the pakcage in the memory for all users.
+	4. Feature that can be implemented only with packages.
+		1. Overloading of subprograms
+		2. Global variables and constants
